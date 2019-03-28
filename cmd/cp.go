6f403cd45b8d70c8807459bafcb9fe7f06db9edf
Jan Martens <jan.martens@rwth-aachen.de>
2019-03-28 21:10:38 +0100

    Make cp command more modular

--- cmd/cp.go
+++ cmd/cp.go
@@ -18,7 +18,9 @@ var (
 		Use:   "cp",
 		Args:  cobra.ExactArgs(2),
 		Short: "Copies old-path to new-path, optionally forcefully, selectively reencrypting.",
-		Run:   copyPassword,
+		Run: func(cmd *cobra.Command, args []string) {
+			copyPasswords(args[0], args[1], ForceCp)
+		},
 	}
 
 	ForceCp bool
@@ -30,20 +32,23 @@ func init() {
 	cpCmd.Flags().BoolVarP(&ForceCp, "force", "f", false, "Forcefully copy password or directory.")
 }
 
-func copyPassword(cmd *cobra.Command, args []string) {
-	fromPath := util.GetPasswordStore() + "/" + args[0]
-	toPath := util.GetPasswordStore() + "/" + args[1]
+func copyPasswords(src, dst string, force bool) {
+	fromPath := util.GetPasswordStore() + "/" + src
+	toPath := util.GetPasswordStore() + "/" + dst
 	// src exists and is a directory
 	if f, e := os.Stat(fromPath); !os.IsNotExist(e) && f.IsDir() {
 		// dst exists and is a directory
-		if f, e := os.Stat(toPath); !os.IsNotExist(e) && f.IsDir() && !ForceCp &&
+		if f, e := os.Stat(toPath); !os.IsNotExist(e) && f.IsDir() && !force &&
 			!util.YesNo(fmt.Sprintf("%s already exists. Do you want to overwrite it?", toPath)) {
 			os.Exit(1)
 		}
 		fmt.Println(fromPath)
 		copy.Copy(fromPath, toPath)
-		gpgID := findGpgID(toPath)
-		recv, _ := getRecipientsFromGpgID(gpgID)
+		recv, e := getRecipientsFromGpgID(findGpgID(toPath))
+		if e != nil {
+			fmt.Println(1)
+			os.Exit(1)
+		}
 		// walk dst directory
 		reEncryptDir(toPath, recv)
 		// src exists and is not a directory
@@ -53,17 +58,20 @@ func copyPassword(cmd *cobra.Command, args []string) {
 			toPath += filepath.Base(fromPath)
 		}
 		// dst exists and is a file and no forcefuly overwriting
-		if f, e := os.Stat(toPath + ".gpg"); !os.IsNotExist(e) && !f.IsDir() && !ForceCp &&
+		if f, e := os.Stat(toPath + ".gpg"); !os.IsNotExist(e) && !f.IsDir() && !force &&
 			!util.YesNo(fmt.Sprintf("%s already exists. Do you want to overwrite it?", toPath+".gpg")) {
 			os.Exit(1)
 		}
 		fmt.Println(fromPath + ".gpg")
 		copy.Copy(fromPath+".gpg", toPath+".gpg")
-		gpgID := findGpgID(toPath + ".gpg")
-		recv, _ := getRecipientsFromGpgID(gpgID)
+		recv, e := getRecipientsFromGpgID(findGpgID(toPath))
+		if e != nil {
+			fmt.Println(1)
+			os.Exit(1)
+		}
 		reEncryptFile(toPath+".gpg", recv)
 	} else {
-		fmt.Printf("Error: %s is not in the password store.\n", args[0])
+		fmt.Printf("Error: %s is not in the password store.\n", src)
 		os.Exit(1)
 	}
 }
