fff109e5aa9d42c12e0058dd3641a4f397fbbaa2
JanMa <git@janma.tk>
2019-07-28 15:59:10 +0200

    sort grep output

--- cmd/grep.go
+++ cmd/grep.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"regexp"
+	"sort"
 	"strings"
 
 	"github.com/spf13/cobra"
@@ -12,12 +13,17 @@ import (
 func grepPasswords(cmd *cobra.Command, args []string) {
 	r := regexp.MustCompile(args[0])
 	all := PasswordStore.ShowAll()
-	for _, e := range all {
-		err := e.Decrypt()
+	names := []string{}
+	for n := range all {
+		names = append(names, n)
+	}
+	sort.Strings(names)
+	for _, n := range names {
+		err := all[n].Decrypt()
 		exitOnError(err)
-		v, _ := e.Value()
+		v, _ := all[n].Value()
 		if r.MatchString(v) {
-			fmt.Printf(util.BoldBlue+"%s:\n"+util.Reset, e.Name)
+			fmt.Printf(util.BoldBlue+"%s:\n"+util.Reset, all[n].Name)
 			for _, l := range strings.Split(v, "\n") {
 				if r.MatchString(l) {
 					fmt.Println(l)
