f103f513d6ea0bdd5cbc0704e0a90e6d8c618c76
JanMa <git@janma.tk>
2019-07-28 16:19:03 +0200

    move otp command to store package

--- cmd/otp.go
+++ cmd/otp.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 	"strings"
 	"time"
 
@@ -10,23 +9,20 @@ import (
 	"github.com/pquerna/otp"
 	"github.com/pquerna/otp/totp"
 	"github.com/spf13/cobra"
-	"gitlab.com/JanMa/go-pass/pkg/util"
 )
 
 func genOtpCode(cmd *cobra.Command, args []string) {
-	root := util.GetPasswordStore() + "/" + args[0] + ".gpg"
-
-	if _, e := os.Stat(root); os.IsNotExist(e) {
-		fmt.Printf("Error: %s is not in the password store.\n", args[0])
-		os.Exit(1)
-	}
-	lines := util.RunCommand("gpg", "-dq", root)
-	u, e := findOtpUrl(lines)
-	exitOnError(e)
-	k, e := otp.NewKeyFromURL(u)
-	exitOnError(e)
-	c, e := totp.GenerateCode(k.Secret(), time.Now())
-	exitOnError(e)
+	e, err := PasswordStore.FindEntry(args[0])
+	exitOnError(err)
+	exitOnError(e.Decrypt())
+	v, _ := e.Value()
+	lines := strings.Split(v, "\n")
+	u, err := findOtpURL(lines)
+	exitOnError(err)
+	k, err := otp.NewKeyFromURL(u)
+	exitOnError(err)
+	c, err := totp.GenerateCode(k.Secret(), time.Now())
+	exitOnError(err)
 	if OtpClip {
 		exitOnError(clipboard.WriteAll(c))
 	} else {
@@ -34,11 +30,11 @@ func genOtpCode(cmd *cobra.Command, args []string) {
 	}
 }
 
-func findOtpUrl(lines []string) (string, error) {
+func findOtpURL(lines []string) (string, error) {
 	for _, l := range lines {
 		if strings.Contains(l, "otpauth://totp") {
 			return l, nil
 		}
 	}
-	return "", fmt.Errorf("OTP secret not found.")
+	return "", fmt.Errorf("otp secret not found")
 }
