84c4fae463fb3660f62e0594298e1337fd8b060f
JanMa <git@janma.tk>
2019-08-19 19:14:00 +0200

    move insert command to store package

--- cmd/insert.go
+++ cmd/insert.go
@@ -4,46 +4,39 @@ import (
 	"bufio"
 	"fmt"
 	"golang.org/x/crypto/ssh/terminal"
-	"io"
 	"os"
-	"os/exec"
-	"path/filepath"
 	"strings"
 
 	"github.com/spf13/cobra"
 	"gitlab.com/JanMa/go-pass/pkg/git"
+	"gitlab.com/JanMa/go-pass/pkg/store"
+	"gitlab.com/JanMa/go-pass/pkg/store/entry"
 	"gitlab.com/JanMa/go-pass/pkg/util"
 )
 
 func insertPassword(cmd *cobra.Command, args []string) {
-	root := util.GetPasswordStore() + "/" + args[0] + ".gpg"
-	if f, e := os.Stat(root); !os.IsNotExist(e) && !f.IsDir() {
-		if ForceInsert || util.YesNo(fmt.Sprintf("An entry already exists for %s. Overwrite it?", args[0])) {
-			exitOnError(os.Remove(root))
-		} else {
-			os.Exit(1)
-		}
+	e, err := PasswordStore.FindEntry(args[0])
+	if err == nil && !ForceInsert &&
+		!util.YesNo(fmt.Sprintf("An entry already exists for %s. Overwrite it?", args[0])) {
+		os.Exit(1)
 	}
+	if e == nil {
+		e = entry.New(args[0], PasswordStore.Path+"/"+args[0]+".gpg")
+		PasswordStore.InsertEntry(e)
+	}
+	var pass string
 	if MultiLine {
 		fmt.Printf("Enter contents of %s and press Ctrl+D when finished:\n\n", args[0])
-		encryptMultiLine(root)
+		pass, err = readMultiLine()
 	} else {
-		encryptPassword(enterPassword(args[0])+"\n", root)
+		pass, err = enterPassword(args[0])
 	}
-	git.AddFile(root, fmt.Sprintf("Add given password for %s to store.", args[0]))
-}
-
-func getRecepientOptsArray() []string {
-	opts := []string{}
-	idFile := util.GetPasswordStore() + "/.gpg-id"
-	i, e := os.Open(idFile)
-	exitOnError(e)
-	s := bufio.NewScanner(i)
-	for s.Scan() {
-		opts = append(opts, "-r")
-		opts = append(opts, s.Text())
-	}
-	return opts
+	exitOnError(err)
+	e.Insert(pass)
+	iD := PasswordStore.FindGpgID(e.Path)
+	recv, _ := store.ParseGpgID(iD)
+	exitOnError(e.Encrypt(recv))
+	git.AddFile(e.Path, fmt.Sprintf("Add given password for %s to store.", args[0]))
 }
 
 func readPassword() (string, error) {
@@ -58,43 +51,28 @@ func readPassword() (string, error) {
 	return p, e
 }
 
-func enterPassword(name string) string {
+func enterPassword(name string) (string, error) {
 	fmt.Printf("Enter password for %s: ", name)
 	pass, e := readPassword()
-	exitOnError(e)
+	if e != nil {
+		return "", e
+	}
 	fmt.Printf("Retype password for %s: ", name)
 	passAgain, e := readPassword()
-	exitOnError(e)
+	if e != nil {
+		return "", e
+	}
 	if pass != passAgain {
-		fmt.Println("Error: the entered passwords do not match.")
-		os.Exit(1)
+		return "", fmt.Errorf("the entered passwords do not match")
 	}
-	return pass
+	return pass + "\n", nil
 }
 
-func encryptPassword(pass, file string) {
-	gpg := exec.Command("gpg",
-		"-e", "-o", strings.ReplaceAll(file, " ", `\ `),
-		"--quiet", "--yes", "--compress-algo=none", "--no-encrypt-to")
-	for _, r := range getRecepientOptsArray() {
-		gpg.Args = append(gpg.Args, r)
-	}
-	stdin, err := gpg.StdinPipe()
-	exitOnError(err)
-	go func() {
-		defer stdin.Close()
-		io.WriteString(stdin, pass)
-	}()
-	os.MkdirAll(filepath.Dir(file), 0755)
-	exitOnError(gpg.Run())
-}
-
-func encryptMultiLine(file string) {
+func readMultiLine() (string, error) {
 	scanner := bufio.NewScanner(os.Stdin)
 	pass := []string{}
 	for scanner.Scan() {
 		pass = append(pass, scanner.Text())
 	}
-	exitOnError(scanner.Err())
-	encryptPassword(strings.Join(pass, "\n")+"\n", file)
+	return strings.Join(pass, "\n") + "\n", scanner.Err()
 }
