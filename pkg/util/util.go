85788783caa86a388287bb0965955e4425ebb3e4
JanMa <git@janma.tk>
2019-08-31 23:20:16 +0200

    cleanup util package

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -5,10 +5,8 @@ import (
 	"fmt"
 	"math/big"
 	"os"
-	"os/exec"
 	"strings"
 
-	homedir "github.com/mitchellh/go-homedir"
 	"golang.org/x/crypto/ssh/terminal"
 )
 
@@ -35,48 +33,6 @@ const (
 	// Reset reset text formating
 	Reset = "\033[0m"
 )
-
-// PrintLine prints a line of output
-func PrintLine(s string) {
-	if i := strings.LastIndex(s, ".gpg"); i != -1 {
-		fmt.Println(s[:i])
-	} else {
-		fmt.Println(s)
-	}
-}
-
-// RunCommand runs a given command and returns the output
-func RunCommand(name string, args ...string) []string {
-	cmd, err := exec.Command(name, args...).CombinedOutput()
-	if err != nil {
-		fmt.Println(string(cmd))
-		defer os.Exit(1)
-	}
-	return strings.Split(string(cmd), "\n")
-}
-
-func getHomeDir() string {
-	home, err := homedir.Dir()
-	if err != nil {
-		fmt.Println(err)
-		defer os.Exit(1)
-	}
-	return home
-}
-
-// GetPasswordStore returns the path to the password store
-func GetPasswordStore() string {
-	env := os.Getenv("PASSWORD_STORE_DIR")
-	if len(env) == 0 {
-		env = getHomeDir() + "/.password-store"
-	}
-	if f, e := os.Stat(env); os.IsNotExist(e) || !f.IsDir() {
-		if err := os.MkdirAll(env, 0755); err != nil {
-			os.Exit(1)
-		}
-	}
-	return env
-}
 
 // YesNo simple Yes or No dialogue
 func YesNo(msg string) bool {
